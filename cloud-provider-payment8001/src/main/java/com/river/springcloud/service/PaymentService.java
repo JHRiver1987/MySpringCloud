0387486dd2d14167f11ee00c1c3ae482bd94c378
JHRiver1987 <jiangheriver@163.com>
2020-05-05 09:16:22 +0800

    1.Eureka 2.Zookeeper

---
+++ cloud-provider-payment8001/src/main/java/com/river/springcloud/service/PaymentService.java
@@ -0,0 +1,11 @@
+package com.river.springcloud.service;
+
+import com.river.springcloud.entities.Payment;
+import org.apache.ibatis.annotations.Param;
+
+public interface PaymentService {
+
+    int create(Payment payment);
+
+    Payment getPaymentById(@Param("id") Long id);
+}
